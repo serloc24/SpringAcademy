4e27f7b19547b63b3293a5143ce51421dbba2770
lopez <serlocol24@gmail.com>
2025-10-02 12:05:53 +0200

    First Commit Spring Initialzr

---
+++ cashcard/src/test/java/com/serloc/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serloc.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
